358545ff523225d57b0531de10d55f3c87ee7129
tldrd0117 <tldrd0117@gmail.com>
2021-10-31 17:50:30 +0900

    apply dask frameworks

--- tests/test_get_stock.py
+++ tests/test_get_stock.py
@@ -2,14 +2,17 @@ import requests
 import json
 
 import pandas as pd
+import numpy as np
 import os.path
 
 import dask.dataframe as dd
 import dask.array as da
 import dask.bag as db
+
+
 # pytest -s test_get_stock.py
-def loadRemoteDataframe(startDate, endDate) :
-    res = requests.get(f"http://52.78.107.218:30004/stock/marcap?market=kosdaq&startDate={startDate}&endDate={endDate}")
+def loadRemoteDataframe(market: str, startDate: str, endDate: str) -> dd.DataFrame :
+    res = requests.get(f"http://52.78.107.218:30004/stock/marcap?market={market}&startDate={startDate}&endDate={endDate}")
     obj = json.loads(res.text)
     columns = []
     assert obj["list"] != None
@@ -17,35 +20,68 @@ def loadRemoteDataframe(startDate, endDate) :
     if obj["count"] > 0:
         columns = obj["list"][0].keys()
     df = pd.DataFrame(obj["list"], columns=columns)
-    df["date"] = pd.to_datetime(df["date"], format="%Y%m%d")
-    df.set_index(["date", "market", "code"], inplace=True)
+    df = dd.from_pandas(df, npartitions=100)
+    df["date"] = dd.to_datetime(df["date"], format="%Y%m%d")
+    # df = df.set_index(["date", "market", "code"], sorted=True)
     return df
 
-def loadLocalframe(market, year):
+def loadLocalDataframe(market: str, year: str) -> dd.DataFrame:
     path = f"/Users/iseongjae/Documents/PersonalProjects/fin-simulation-module/src/h5/marcap/{market}_{year}.hdf"
     if os.path.isfile(path):
-        return pd.read_hdf(path)
+        return dd.read_hdf(path, key="marcap", mode="r")
     else:
-        return None
+        return dd.from_pandas(pd.DataFrame(), npartitions=1)
 
-def saveDataframe(df):
-    years = pd.DatetimeIndex(df.index.get_level_values(0)).year.drop_duplicates()
-    markets = df.index.get_level_values(1).drop_duplicates()
-    print(years, markets)
+def saveLocalDataframe(targetDf: dd.DataFrame, market: str, year: str) -> bool:
+    try:
+        df = loadLocalDataframe(market, year)
+        path = f"/Users/iseongjae/Documents/PersonalProjects/fin-simulation-module/src/h5/marcap/{market}_{year}.hdf"
+        if len(df) > 0:
+            print(df)
+            dt = df["date"].drop_duplicates().map_partitions(np.asarray, dtype=np.datetime64)
+            targetDt = targetDf["date"].drop_duplicates().map_partitions(np.asarray, dtype=np.datetime64)
+            # 기존의 날짜와 비교하여 없는 부분만 concat
+            diff = np.setdiff1d(targetDt, dt)
+            resultDf = dd.concat([df, targetDf[targetDf["date"].isin(diff)]]).map_partitions(lambda df: df.sort_values(['date', 'market','code']))
+            resultDf.to_hdf(path, key="marcap", mode="w", min_itemsize=100)
+        else:
+            targetDf.to_hdf(path, key="marcap", mode="w", min_itemsize=100)
+    except Exception as e:
+        print(e)
+        return False
+    return True
+
+    
+
+def saveDataframe(df: dd.DataFrame):
+    df = df.persist()
+    years = df["date"].dt.year.drop_duplicates()
+    markets = df["market"].drop_duplicates()
+
     for year in years:
         for market in markets:
-            partDf = loadLocalframe(market, year)
-            # partDf = df.loc[pd.IndexSlice[str(year), market, :]]
+            partDf = df[df["date"].dt.year == year][df["market"]==market]
+            saveLocalDataframe(partDf, market, year)
 
     # for year in years:
     # df.loc[]
     # print(df.loc["2020"])
 
 def test():
-    df = loadRemoteDataframe("20211029", "20211029")
-    df2 = loadRemoteDataframe("20201029", "20201029")
-    df3 = loadRemoteDataframe("20191029", "20191029")
-    totalDf = pd.concat([df, df2, df3]).sort_index(level=["date","market","code"])
-    saveDataframe(totalDf)
+    print(loadLocalDataframe("kosdaq", "2019").compute())
+    # print(loadLocalDataframe("kosdaq", "2020").compute())
+    # print(loadLocalDataframe("kosdaq", "2021").compute())
+    # print(loadLocalDataframe("kospi", "2019").compute())
+    # print(loadLocalDataframe("kospi", "2020").compute())
+    # print(loadLocalDataframe("kospi", "2021").compute())
+
+    # df = loadRemoteDataframe("kosdaq", "20211023", "20211025")
+    # df2 = loadRemoteDataframe("kosdaq", "20201027", "20201029")
+    # df3 = loadRemoteDataframe("kosdaq", "20191027", "20191029")
+    # df4 = loadRemoteDataframe("kospi", "20211027", "20211029")
+    # df5 = loadRemoteDataframe("kospi", "20201027", "20201029")
+    # df6 = loadRemoteDataframe("kospi", "20191027", "20191029")
+    # totalDf = dd.concat([df]).map_partitions(lambda df: df.sort_values(['date', 'market','code']))
+    # saveDataframe(totalDf)
     
     # print(df.loc[df.index == ("20211029","kosdaq","000250")])
