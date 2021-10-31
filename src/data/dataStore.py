13b250fbf0362f265326ab239f5c2a7a2a078ac8
tldrd0117 <tldrd0117@gmail.com>
2021-10-31 17:51:38 +0900

    apply dask frameworks

--- src/data/dataStore.py
+++ src/data/dataStore.py
@@ -5,6 +5,7 @@ from typing import Dict
 import requests
 import json
 import pandas as pd
+import os.path
 
 class MarcapStore:
     startDate: str
@@ -20,11 +21,33 @@ class MarcapStore:
         self.localPath = localPath
         self.remotePath = remotePath
 
-    def loadLocal():
+    def loadLocal(self) -> pd.DataFrame:
+        path = f"/Users/iseongjae/Documents/PersonalProjects/fin-simulation-module/src/h5/marcap/{self.market}_{self.year}.hdf"
+        if os.path.isfile(path):
+            return pd.read_hdf(path, key="marcap", mode="r")
+        else:
+            return pd.DataFrame()
+    
+    def saveLocalPartitionOfYear(self, targetDf: pd.DataFrame, market: str, year: str) -> bool:
+        try:
+            df: pd.DataFrame = self.loadLocal(market, year)
+            path = f"/Users/iseongjae/Documents/PersonalProjects/fin-simulation-module/src/h5/marcap/{market}_{year}.hdf"
+            if not df.empty:
+                dt: pd.DatetimeIndex = df.index.get_level_values(0).drop_duplicates()
+                targetDt: pd.DatetimeIndex = targetDf.index.get_level_values(0).drop_duplicates()
+                # 기존의 날짜와 비교하여 없는 부분만 concat
+                diff =targetDt.difference(dt)
+                resultDf: pd.DataFrame = pd.concat([df, targetDf.loc[diff]]).sort_index(level=["date","market","code"])
+                resultDf.to_hdf(path, key="marcap", mode="w")
+            else:
+                targetDf.to_hdf(path, key="marcap", mode="w")
+        except Exception as e:
+            print(e)
+            return False
+        return True
+    
+    def saveLocal(self) -> bool:
         pass
-
-    def saveLocal(self, marcapDf):
-        marcapDf
         
 
     def loadRemote(self, isSaveLocal = True):
@@ -35,7 +58,8 @@ class MarcapStore:
         df = pd.DataFrame(obj["list"], columns=columns)
         df.set_index(["date", "market", "code"], inplace=True)
 
-        # if isSaveLocal:
+        if isSaveLocal:
+            self.saveLocal(df, )
 
 
 
